b3c89319d591f158528228508ebe35d9805b1cc2
Megan Corletti <corlettim@allegheny.edu>
2020-05-20 13:50:16 -0400

    add code challenge

--- python-for-beginners/05 - Numeric variables/code_challenge.py
+++ python-for-beginners/05 - Numeric variables/code_challenge.py
@@ -1,6 +1,11 @@
 # Ask a user to enter a number
 # Ask a user to enter a second number
 # Calculate the total of the two numbers added together
-# Print 'first number + second number = answer' 
+# Print 'first number + second number = answer'
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
+
+first = input("Please enter a number: ")
+second = input("Please enter another number: ")
+total = float(first) + float(second)
+print(first, "+", second, "=", total)
--- python-for-beginners/05 - Numeric variables/numbers_treated_as_strings.py
+++ python-for-beginners/05 - Numeric variables/numbers_treated_as_strings.py
@@ -2,8 +2,8 @@
 
 # since the input function returns a string, the variables it populates
 # will hold string values
-first_num = input('Enter first number ')
-second_num = input('Enter second number ')
+first_num = input('Enter first number: ')
+second_num = input('Enter second number: ')
 
 # Because first_num and second_num are string variables the + operator
 # concatenates them just like concatenating first_name and last_name
