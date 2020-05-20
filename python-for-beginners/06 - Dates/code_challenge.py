8337688b344efb4f098ce028acc4b560ea8983ba
Megan Corletti <corlettim@allegheny.edu>
2020-05-20 14:29:42 -0400

    add code challenge

--- python-for-beginners/06 - Dates/code_challenge.py
+++ python-for-beginners/06 - Dates/code_challenge.py
@@ -2,3 +2,21 @@
 # print yesterday's date
 # ask a user to enter a date
 # print the date one week from the date entered
+
+from datetime import datetime, timedelta
+
+date_now = datetime.now()
+print(date_now)
+
+#timedelta is used for dates other than today
+new_date = timedelta(days=1)
+day_yesterday = date_now - new_date
+print("Yesterday was: ", str(day_yesterday))
+
+user_day = input("Please enter a date (dd/mm/yyyy): ")
+user_day = datetime.strptime(user_day, '%d/%m/%Y')
+print(str(user_day))
+
+new_user_day = timedelta(weeks=1)
+week_yesterday = user_day + new_user_day
+print("Last week was: ", str(week_yesterday))
