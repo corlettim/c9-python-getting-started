3a9e211b550d481f2b86ce403c6c5339b374c7fd
Megan Corletti <corlettim@allegheny.edu>
2020-05-26 12:42:12 -0400

    add code challenge

--- python-for-beginners/14 - Function parameters/code_challenge.py
+++ python-for-beginners/14 - Function parameters/code_challenge.py
@@ -9,8 +9,32 @@
 # Test your function using named notation passing in only the numbers 6 and 4
 # Should return 10
 #
-# Test your function using named notation with the values 6,4, subtract 
+# Test your function using named notation with the values 6,4, subtract
 # Should return 2
-# 
-# BONUS: Test your function with the values 6, 4 and divide 
+#
+# BONUS: Test your function with the values 6, 4 and divide
 # Have your function return an error message when invalid values are received
+
+
+def calculator(first, second, operation = "add"):
+    if operation == "add":
+        add = float(first_number) + float(second_number)
+        return add
+    elif operation == "subtract":
+        sub = float(first_number) - float(second_number)
+        return sub
+    else:
+        error = "That operation is not possible"
+        return error
+
+first_number = float(input("Please enter a number: "))
+second_number = float(input("Please enter another number: "))
+
+# first attempt was to ask for which operation they would like
+# however that does not work because they will have to have the option of add or subtract
+# and therefore there would be no default of addition
+# this we must pass the operation ourselves ad coders rather than it being left up to the user
+
+print("Adding: ", calculator(first_number, second_number))
+print("Subtracting: ", calculator(first_number, second_number, operation="subtract"))
+print("ERROR: ", calculator(first_number, second_number, operation="divide"))
